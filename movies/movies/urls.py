42ca4d76e18f5ec1ba74f649a411907014400222
Patrice Remy Lou D Virata <trish.virata@gmail.com>
2025-02-16 11:18:25 -0600

    initial

--- movies/movies/urls.py
+++ movies/movies/urls.py
@@ -15,14 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
+from django.conf import settings
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from api import views
 from api.views import RegisterView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth', include('rest_framework.urls')), #allow login/logout for non-admin from local server page
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),  # serve media files when deployed
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    # serve static files when deployed
+    path('api-auth', include('rest_framework.urls')),  # allow login/logout for non-admin from local server page
     path('api/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', views.movie_list),
